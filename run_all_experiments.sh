9295584afd75d5efa6e6a5c155e2bcfa11cbbcef
abdulmomen.ghalkha <abdulmomen.ghalkha@gmail.com>
2024-11-12 14:31:08 +0200

    Modify the bash script

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Run each Python command sequentially
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.01 --Ct 0.1
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.01 --Ct 0.4
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.01 --Ct 0.5
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.01 --Ct 0.9
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.01 --Ct 0.1
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.01 --Ct 0.4
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.01 --Ct 0.5
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.01 --Ct 0.9
 
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.1 --Ct 0.4
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.01 --Ct 0.4
-python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.001 --Ct 0.4
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.1 --Ct 0.4
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.01 --Ct 0.4
+python main.py --dataset school --algorithm Sheaf-FMTL-subgraph --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.001 --Ct 0.4
 
 
-python main.py --dataset school --algorithm Sheaf-FMTL --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.8 --num-rounds 200 --times 5 --beta 0.001 --Ct 0.4
+python main.py --dataset school --algorithm Sheaf-FMTL --alpha 0.005 --eta 0.05 --lambda-reg 0.02 --factor 0.4 --num-rounds 200 --times 1 --beta 0.001 --Ct 0.4
 
 python main.py --dataset school --algorithm dFedU
 
